c590e1c51da301762926c5122da387de3b0faafa
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-03-26 04:37:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,14 @@ class Dog
     @breed = new_breed
   end
 
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end  
+
   def breed=(new_breed)
     @breed = new_breed
   end
