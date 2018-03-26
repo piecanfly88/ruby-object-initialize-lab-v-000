34feff46d6ef5df6151e1b5aca27fa2065e16dcd
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-03-26 07:57:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  def initialize(new_name, new_breed = "Mutt")
+    @name = new_name
+    @breed = new_breed
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+  def breed
+    @breed
+  end
+end
